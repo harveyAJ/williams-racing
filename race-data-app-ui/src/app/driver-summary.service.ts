a4a8dfd0df317fee5cdfd13fad5471ada8b91591
Valentin A Roy <valentin.a.roy@collins.com>
2025-09-24 02:51:58 +0100

    add the angular app

---
+++ race-data-app-ui/src/app/driver-summary.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface DriverSummary {
+  driverId: number;
+  driverRef: string;
+  forename: string;
+  surname: string;
+  dob: string;
+  nationality: string;
+  totalPodiums: number;
+  totalRaces: number;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DriverSummaryService {
+  private baseUrl = 'http://localhost:5002';
+
+  constructor(private http: HttpClient) {}
+
+  getSummaries(): Observable<DriverSummary[]> {
+    return this.http.get<DriverSummary[]>(`${this.baseUrl}/driver-summary`);
+  }
+}
